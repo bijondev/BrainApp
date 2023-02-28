dc98278f202c5ed67b8f94e9b1aaaf4e3b521c2d
Bijoin Krishna bairagi <bijon.bairagi@gmail.com>
2023-02-28 16:08:05 +0600

    game problem

--- components/screens/GameScreen.js
+++ components/screens/GameScreen.js
@@ -1,25 +1,37 @@
 import { useState } from "react";
-import Title from '../ui/Title';
-import Colors from '../ui/colors';
-
 import {
   StyleSheet,
   TextInput,
   View,
   Text,
-  Alert,
-  SafeAreaView,
+  Alert
 } from "react-native";
+import Title from '../ui/Title';
+import Colors from '../ui/Colors';
+import NumberContainer from "./NumberContainer";
 
-function GameScreen() {
+
+function generateRandomNumber(min, max, exclude){
+    const rnNum = Math.floor(Math.random()+(max-min))+min;
+    if(rnNum == exclude){
+        return generateRandomNumber(min, max, exclude);
+    }
+    else{
+        return rnNum;
+    }
+}
+
+function GameScreen({userNumber}) {
+  const initialGuess = generateRandomNumber(1,100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
-    <View style={styles.screen}>
-      <Title>Opponent's Guess</Title>
+    <View style={styles.screen} >
+      <Title>Opponents Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
-        {/* + - */}
       </View>
-      {/* <View>Log Rounds</View> */}
     </View>
   );
 }
@@ -38,5 +50,5 @@ const styles = StyleSheet.create({
     textAlign: "center",
     borderWidth: 2,
     borderColor: Colors.accent500,
-  },
+  }
 });
--- components/ui/colors.js
+++ components/ui/colors.js
@@ -1,7 +1,6 @@
 const Colors = {
-    primary500: '#72063c',
-    primary600: '#640233',
-    primary800: '#3b021f',
-    paccent500: '#ddb52f',
-
+  primary500: "#72063c",
+  primary600: "#640233",
+  primary800: "#3b021f",
+  accent500: "#ddb52f",
 };
