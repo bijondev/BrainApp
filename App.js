4e33da09fb523f3923cfa64b213125db34b4988f
Bijoin Krishna bairagi <bijon.bairagi@gmail.com>
2023-03-01 17:57:36 +0600

    calculation problem

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default function App() {
   }
   let screen = <StartGame onpickedNumber={pickedNumberhandeler} />;
   if (userNumber) {
-    screen = <GameScreen />;
+    screen = <GameScreen userNumber={userNumber} />;
   }
 
   return (
--- components/screens/GameScreen.js
+++ components/screens/GameScreen.js
@@ -1,36 +1,68 @@
 import { useState } from "react";
-import {
-  StyleSheet,
-  TextInput,
-  View,
-  Text,
-  Alert
-} from "react-native";
-import Title from '../ui/Title';
-import Colors from '../ui/Colors';
+import { StyleSheet, TextInput, View, Text, Alert } from "react-native";
+import Title from "../ui/Title";
+import Colors from "../ui/Colors";
 import NumberContainer from "./NumberContainer";
+import PrimaryButton from "../ui/PrimaryButton";
 
+let min = 1;
+let max = 100;
 
-function generateRandomNumber(min, max, exclude){
-    const rnNum = Math.floor(Math.random()+(max-min))+min;
-    if(rnNum == exclude){
-        return generateRandomNumber(min, max, exclude);
-    }
-    else{
-        return rnNum;
-    }
+function generateRandomNumber(min, max, exclude) {
+  const rnNum = Math.floor(Math.random() * (max - min)) + min;
+  console.log(rnNum);
+  if (rnNum == exclude) {
+    return generateRandomNumber(min, max, exclude);
+  } else {
+    return rnNum;
+  }
 }
 
-function GameScreen({userNumber}) {
-  const initialGuess = generateRandomNumber(1,100, userNumber);
+function GameScreen(userNumber) {
+  const initialGuess = generateRandomNumber(min, max, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
+  function nextHandeler(direction) {
+    console.log("nextHandeler>>>>>>>>>>>>>>>>>>>>");
+    console.log(currentGuess , userNumber.userNumber);
+    if (
+      (direction === "lower" && currentGuess < userNumber.userNumber) ||
+      (direction === "greater" && currentGuess > userNumber.userNumber)
+    ) {
+      Alert.alert("Sorry!", "LIE!!!"),
+        [
+          {
+            text: "Sorry!",
+            style: "cancel",
+          },
+        ];
+      return;
+    }
+    if (direction == "lower") {
+      max = currentGuess;
+    } else {
+      min = currentGuess + 1;
+    }
+
+    console.log(min, max, currentGuess);
+
+    const newNumber = generateRandomNumber(min, max, currentGuess);
+    setCurrentGuess(newNumber);
+  }
   return (
-    <View style={styles.screen} >
-      <Title>Opponents Guess</Title>
-      <NumberContainer>{currentGuess}</NumberContainer>
+    <View style={styles.screen}>
+      <Title> Opponents Guess </Title>
+      <NumberContainer> {currentGuess} </NumberContainer>
       <View>
-        <Text>Higher or lower?</Text>
+        <Text> Higher or lower ? </Text>
+      </View>
+      <View>
+        <PrimaryButton onPress={nextHandeler.bind(this, "lower")}>
+          -
+        </PrimaryButton>
+        <PrimaryButton onPress={nextHandeler.bind(this, "greater")}>
+          +
+        </PrimaryButton>
       </View>
     </View>
   );
@@ -50,5 +82,5 @@ const styles = StyleSheet.create({
     textAlign: "center",
     borderWidth: 2,
     borderColor: Colors.accent500,
-  }
+  },
 });
